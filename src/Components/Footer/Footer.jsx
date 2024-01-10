a912a1bb9c70d9f56609fb0dadc779ad7b01c8a8
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2024-01-10 21:23:42 +0330

    feat : added Footer component

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+// Footer.jsx
+
+import styles from "./footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div>kkkk</div>
+      <div>bbbb</div>
+    </div>
+  );
+};
+
+export default Footer;
