c5bb5da63175d2e6c9309e6025c4a11abd3e3afb
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2024-01-10 21:24:36 +0330

    feat : add Header component

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,78 @@
+import { Menu } from "antd";
+import { HomeFilled } from "@ant-design/icons";
+import styles from "./header.module.css";
+import { useNavigate } from "react-router-dom";
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  const menuOnClick = (item) => {
+    navigate(`${item.key}`);
+  };
+
+  return (
+    <div className={styles.container}>
+      <Menu
+        onClick={menuOnClick}
+        className={styles.menu}
+        mode="horizontal"
+        items={[
+          {
+            label: <HomeFilled />,
+            key: ""
+          },
+          {
+            label: "Men",
+            key: "men",
+            children: [
+              {
+                label: "Men's Shirts",
+                key: "mens-shirts"
+              },
+              {
+                label: "Men's Shoes",
+                key: "mens-shoes"
+              },
+              {
+                label: "Men's Watches",
+                key: "mens-watches"
+              }
+            ]
+          },
+          {
+            label: "Women",
+            key: "women",
+            children: [
+              {
+                label: "Women's Dresses",
+                key: "womens-dresses"
+              },
+              {
+                label: "Women's Shoes",
+                key: "womens-shoes"
+              },
+              {
+                label: "Women's Watches",
+                key: "womens-watches"
+              },
+              {
+                label: "Women's Bags",
+                key: "womens-bags"
+              },
+              {
+                label: "Women's Jewellery",
+                key: "womens-jewellery"
+              }
+            ]
+          },
+          {
+            label: "Fragrances",
+            key: "fragrances"
+          }
+        ]}
+      />
+    </div>
+  );
+};
+
+export default Header;
