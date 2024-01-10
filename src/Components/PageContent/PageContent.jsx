753d26c212759d9be0e793fb85cd21cf94f2ea0a
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2024-01-10 21:25:33 +0330

    feat : added PageContent component

---
+++ src/Components/PageContent/PageContent.jsx
@@ -0,0 +1,11 @@
+import AppRouts from "../../Routs/AppRouts";
+import styles from "./pagecontent.module.css";
+const PageContent = () => {
+  return (
+    <div className={styles.container}>
+      <AppRouts />
+    </div>
+  );
+};
+
+export default PageContent;
