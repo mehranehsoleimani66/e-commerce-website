e6c3b029e135709a6c1f95fb9fa36da3b0b990a7
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2024-01-12 17:16:11 +0330

    chore: the route for the Home page has been updated. All products displayed on the home page.

--- src/Routs/AppRouts.jsx
+++ src/Routs/AppRouts.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
+// import Home from "../pages/Home";
 import Category from "../pages/category";
 
 const AppRouts = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Category />} />
       <Route path="/:categoryId" element={<Category />} />
     </Routes>
   );
