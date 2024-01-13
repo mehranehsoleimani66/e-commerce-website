592957335e9d6022b45095857f45c854660032cf
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2024-01-13 22:57:19 +0330

    feat : defined and export getCart( ) after getting data from api

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -7,6 +7,9 @@ export const getProductsByCategory = (category) => {
     (res) => res.json()
   );
 };
+export const getCart = () => {
+  return fetch("https://dummyjson.com/carts/1").then((res) => res.json());
+};
 
 export const addToCart = (id) => {
   return fetch("https://dummyjson.com/carts/add", {
