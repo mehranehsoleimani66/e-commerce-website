ac5e5ece5eb8efaf24c9f7a3304e943d4df7d722
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2024-01-13 22:48:56 +0330

    feat : made CartItems by antd components and displayed items by fetching from api in cartItems component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,9 @@
-import { Menu } from "antd";
-import { HomeFilled } from "@ant-design/icons";
+import { Menu, Typography, Badge, Drawer, Table, InputNumber } from "antd";
+import { HomeFilled, ShoppingCartOutlined } from "@ant-design/icons";
 import styles from "./header.module.css";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getCart } from "../../API";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -71,6 +73,85 @@ const Header = () => {
           }
         ]}
       />
+      <Typography.Title>Mehii Shop</Typography.Title>
+      <AppCart />
+    </div>
+  );
+};
+const AppCart = () => {
+  const [openDraw, setOpenDraw] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+  useEffect(() => {
+    getCart().then((res) => {
+      setCartItems(res.products);
+    });
+  }, []);
+
+  return (
+    <div>
+      <Badge
+        count={9}
+        className={styles.cartIcon}
+        onClick={() => setOpenDraw(true)}
+      >
+        <ShoppingCartOutlined />
+      </Badge>
+      <Drawer
+        open={openDraw}
+        onClose={() => setOpenDraw(false)}
+        title="your cart"
+        contentWrapperStyle={{ width: 500 }}
+      >
+        <Table
+          pagination={false}
+          columns={[
+            { title: "Title", dataIndex: "title" },
+            {
+              title: "Price",
+              dataIndex: "price",
+              render: (value) => {
+                return <span>${value}</span>;
+              }
+            },
+            {
+              title: "Quantity",
+              dataIndex: "quantity",
+              render: (value, record) => {
+                return (
+                  <InputNumber
+                    min={0}
+                    defaultValue={value}
+                    onChange={(value) => {
+                      setCartItems((pre) =>
+                        pre.map((cart) => {
+                          if (record.id === cart.id) {
+                            cart.total = cart.price * value;
+                          }
+                          return cart;
+                        })
+                      );
+                    }}
+                  ></InputNumber>
+                );
+              }
+            },
+            {
+              title: "Total",
+              dataIndex: "total",
+              render: (value) => {
+                return <span>${value}</span>;
+              }
+            }
+          ]}
+          dataSource={cartItems}
+          summary={(data) => {
+            const total = data.reduce((a, b) => {
+              return a + b.total;
+            }, 0);
+            return <span>Total: ${total}</span>;
+          }}
+        />
+      </Drawer>
     </div>
   );
 };
