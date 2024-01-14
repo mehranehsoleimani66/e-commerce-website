4e8018d45a8691c7236d3bd5bc0ec5ed9094af6a
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2024-01-14 20:45:22 +0330

    feat : added some links in footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,22 @@
 // Footer.jsx
 
 import styles from "./footer.module.css";
-
+import { Typography } from "antd";
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div>kkkk</div>
-      <div>bbbb</div>
+      <Typography.Link href="https://www.google.com" target={"_blank"}>
+        Privacy Policy
+      </Typography.Link>
+      <Typography.Link href="https://www.google.com" target={"_blank"}>
+        Terms & Conditions
+      </Typography.Link>
+      <Typography.Link href="https://www.google.com" target={"_blank"}>
+        Return Policy
+      </Typography.Link>
+      <Typography.Link href="tel:+123456789" target={"_blank"}>
+        +123456789
+      </Typography.Link>
     </div>
   );
 };
